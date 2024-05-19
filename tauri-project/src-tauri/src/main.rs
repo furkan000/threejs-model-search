a133c8fb819cdd07627a8687f3b239c0602a42da
Furkan <tas.furkan@outlook.com>
2024-05-19 18:03:21 +0200

    copied code over to tauri, simple file creation example

--- tauri-project/src-tauri/src/main.rs
+++ tauri-project/src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn write_to_file(path: &str, contents: &str) -> Result<(), String> {
+    std::fs::write(path, contents).map_err(|e| e.to_string())
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![write_to_file])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- tauri-project/src/routes/+page.svelte
+++ tauri-project/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import MainView from "$lib/MainView.svelte";
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  async function saveFile() {
+    const path = `C:\\Users\\Furkan\\Desktop\\file.txt`; // specify your file path
+    const contents = "Hello, world!";
+
+    try {
+      await invoke("write_to_file", { path, contents });
+      console.log("File written successfully");
+    } catch (error) {
+      console.error("Failed to write file", error);
+    }
+  }
+
+  saveFile();
+</script>
+
+<MainView></MainView>
