ec81ce251ccbf46af2a409df00cab90d46ddfd81
Furkan <tas.furkan@outlook.com>
2024-05-23 10:24:57 +0200

    refactor and work on search

--- tauri-project/src/routes/upload-rag/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import Upload from "$lib/Upload.svelte";
-  import UploadFileIcon from "$lib/Icons/UploadFileIcon.svelte";
-</script>
-
-<Upload folder="files" icon={UploadFileIcon} title="Upload Documents" uploadTitle="Document Upload" fileTitle="Uploaded Documents" />
--- tauri-project/src/routes/upload-three-dim/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Upload from "$lib/Upload.svelte";
-  import UploadFileIcon from "$lib/Icons/UploadFileIcon.svelte";
-  import UploadThreeDim from "$lib/Icons/UploadThreeDim.svelte";
-
-  let file;
-</script>
-
-<Upload folder="threedim" icon={UploadThreeDim} title="Upload Model" uploadTitle="Model Upload" fileTitle="Uploaded Models" showExportToThree></Upload>
